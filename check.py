f1c7cd4e0da60b9e7daf112393a1de327b1112fb
Gabor Szabo <gabor@szabgab.com>
2022-12-01 10:04:20 +0200

    fix the check code

--- check.py
+++ check.py
@@ -1,14 +1,33 @@
-import yaml
 import os
-import shutils
+from contextlib import contextmanager
+import tempfile
+import shutil
+import yaml
+
+@contextmanager
+def tmpdir():
+    dd = tempfile.mkdtemp()
+    try:
+        yield dd
+    finally:
+        shutil.rmtree(dd)
+
 
 repos = [
     'https://github.com/szabgab/github-actions-python/',
     'https://github.com/szabgab/github-actions-perl-build/'
 ]
 
+error = False
 for repo in repos:
-    temp = 'temp'
-    if os.path.exists(temp):
-        shutil.rmdir(temp)
-    os.system("git clone repo ${temp}")
+    print(repo)
+    with tmpdir() as temp:
+        print(temp)
+        os.system(f"git clone {repo} {temp}")
+        yaml_file = os.path.join(temp, '.github', 'workflows', 'ci.yml')
+        if not os.path.exists(yaml_file):
+            error = True
+            print(f"Yaml file '{yaml_file}' missing from {repo}")
+
+if error:
+    exit("There was an error")
